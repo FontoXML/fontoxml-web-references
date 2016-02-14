37978302e955c3982f61211c66fd09928734b5f2
devatwork <b.willems@premotion.nl>
2016-02-14 15:05:22 +0100

    1.1.0 FXC-519 Allow overriding of operation data

--- RELEASE.md
+++ RELEASE.md
@@ -1,5 +1,9 @@
 # fontoxml-references-web
 
+## 1.1.0
+
+Make copy overridable for the reference-web-insert-modal and reference-web-edit-modal operations.
+
 ## 1.0.1
 
 Minify JSON files in build.
--- src/install.js
+++ src/install.js
@@ -1,26 +1,18 @@
 define([
 	'fontoxml-modular-ui/uiManager',
-	'fontoxml-operations/operationsManager',
 
 	'./ui/WebReferenceModalController',
-	'./ui/createUiWebReferencePopoverDirective',
-
-	'json!./sx/operations.json'
+	'./ui/createUiWebReferencePopoverDirective'
 ], function (
 	uiManager,
-	operationsManager,
 
 	WebReferenceModalController,
-	createUiWebReferencePopoverDirective,
-
-	operationsJson
+	createUiWebReferencePopoverDirective
 	) {
 	'use strict';
 
 	return function install () {
 		uiManager.addController('WebReferenceModalController', WebReferenceModalController);
 		uiManager.addDirective('uiWebReferencePopover', createUiWebReferencePopoverDirective);
-
-		operationsManager.addOperations(operationsJson);
 	};
 });
