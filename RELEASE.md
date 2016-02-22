8d1b7d81c30a4a7ff855de8606ccec4800526c3b
Thomas Gravekamp <thomas.gravekamp@fontoxml.com>
2016-02-22 14:02:15 +0100

    FXC-447 Make web link validation less strict to allow hostnames

--- RELEASE.md
+++ RELEASE.md
@@ -1,5 +1,9 @@
 # fontoxml-references-web
 
+## next (1.1.1)
+
+Make web link validation less strict to allow hostnames
+
 ## 1.1.0
 
 Make copy overridable for the reference-web-insert-modal and reference-web-edit-modal operations.
--- src/ui/WebReferenceModalController.js
+++ src/ui/WebReferenceModalController.js
@@ -15,9 +15,8 @@ define([
 		// Does not allow hostnames (lacking TLD)
 		$scope.templateData.urlPattern = new RegExp(
 			'^(' +
-
 			'(https?:\\/\\/)?' + // protocol
-			'((([a-z\\d]([a-z\\d-]*[a-z\\d])?)\\.)+[a-z]{2,}|' + // domain name
+			'(([a-z\\d]([a-z\\d-]*[a-z\\d])?(\\.[a-z\\d]([a-z\\d-]*[a-z\\d])?)*)|' + // domain name
 			'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
 			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
 			'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
