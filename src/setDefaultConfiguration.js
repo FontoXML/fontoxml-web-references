671bd90634d89e5332cc28b3a032d229a09a09b8
Stef Busking <stef.busking@gmail.com>
2017-10-03 17:24:01 +0200

    Document configuration values

--- src/setDefaultConfiguration.js
+++ src/setDefaultConfiguration.js
@@ -6,12 +6,26 @@ define([
 	'use strict';
 
 	return function () {
-		// By default: turn validity checking just about off.
+		/**
+		 * Set the regular expression used to check the validity of web references
+		 *
+		 * Defaults to a very forgiving check.
+		 *
+		 * @const  {RegExp}  web-reference-url-validity-regular-expression
+		 * @category  configuration
+		 */
 		configurationManager.setDefault('web-reference-url-validity-regular-expression', /^(https?|ftp):\/\/[^\s]+$/);
 
 		var emailProtocol = 'mailto:',
 		emailUser = '[a-z0-9!#$%&\'*+/=?^_`{|}~\\-]+(\\.[a-z0-9!#$%&\'*+/=?^_`{|}~\\-]+)*',
 		emailDomain = '([a-z0-9]([a-z0-9-]*[a-z0-9])?\\.)*([a-z0-9]([a-z0-9-]*[a-z0-9]))?';
+
+		/**
+		 * Set the regular expression used to check the validity of e-mail references.
+		 *
+		 * @const  {RegExp}  web-reference-email-validity-regular-expression
+		 * @category  configuration
+		 */
 		configurationManager.setDefault('web-reference-email-validity-regular-expression', new RegExp('^(' + emailProtocol + emailUser + '@' + emailDomain + ')$', 'i'));
 	};
 });
