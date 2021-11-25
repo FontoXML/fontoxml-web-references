054b3cd6eaa8bcb561adebe477eebb01588f8759
David Sánchez Jiménez <david.sanchez@liones.nl>
2021-11-25 11:08:15 +0100

    DEV-12068 Publish individual configurationManager values.

--- src/setDefaultConfiguration.ts
+++ src/setDefaultConfiguration.ts
@@ -1,14 +1,6 @@
 import configurationManager from 'fontoxml-configuration/src/configurationManager';
 
 export default function (): void {
-	/**
-	 * Set the regular expression used to check the validity of web references
-	 *
-	 * Defaults to a very forgiving check.
-	 *
-	 * @const  {RegExp}  web-reference-url-validity-regular-expression
-	 * @category  configuration
-	 */
 	configurationManager.setDefault(
 		'web-reference-url-validity-regular-expression',
 		/^(https?|ftp):\/\/[^\s]+$/
@@ -20,12 +12,6 @@ export default function (): void {
 	const emailDomain =
 		'([a-z0-9]([a-z0-9-]*[a-z0-9])?\\.)*([a-z0-9]([a-z0-9-]*[a-z0-9]))?';
 
-	/**
-	 * Set the regular expression used to check the validity of e-mail references.
-	 *
-	 * @const  {RegExp}  web-reference-email-validity-regular-expression
-	 * @category  configuration
-	 */
 	configurationManager.setDefault(
 		'web-reference-email-validity-regular-expression',
 		new RegExp(`^(${emailProtocol}${emailUser}@${emailDomain})$`, 'i')
